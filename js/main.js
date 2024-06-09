9a6816ec307e78a86f6f426aca049abcfe63b1de
abdullah169-eng <abdullahabdo108@gmail.com>
2024-06-09 18:05:22 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed ('#typed', {
+
+    strings: ['Designer', 'Developer', 'Freelancer', 'Photographer'],
+    loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000,
+        showCursor: true,
+  });
